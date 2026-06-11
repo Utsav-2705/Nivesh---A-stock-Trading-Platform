2c1c6d728d063dd13454380b58a8d74c28c827cf
Utsav-2705 <utsavsrivastava2705@gmail.com>
2026-06-12 00:31:41 +0530

    change in menu

--- dashboard/src/components/Menu.js
+++ dashboard/src/components/Menu.js
@@ -100,7 +100,7 @@ const Menu = () => {
          <li>
   <Link
     style={{ textDecoration: "none" }}
-    to="https://nivesh-a-stock-trading-platform-baf.vercel.app/login"
+    to="https://nivesh-a-stock-trading-platform.vercel.app/login"
   >
     <p className={menuClass}>Logout</p>
   </Link>
